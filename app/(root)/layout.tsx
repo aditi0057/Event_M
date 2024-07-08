92eb4a753595ac4ef3ae41143eb5b0331d77030e
aditi0057 <aaditi_be22@thapar.edu>
2024-07-08 10:37:33 +0530

    draft1

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
-import { ClerkProvider } from "@clerk/nextjs";
-import "./globals.css";
+import Header from "@/components/shared/Header";
+import Footer from "@/components/shared/Footer";
 const poppins = Poppins({ 
   subsets: ["latin"],
   weight: ['400','500','600','700'],
@@ -13,17 +13,16 @@ export const metadata: Metadata = {
   title: "Event_Management",
   description: "For Event management",
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-    <html lang="en">
-      <body className={poppins.className}>{children}</body>
-    </html>
-    </ClerkProvider>
+       <div className="flex h-screen flex-col"> 
+        <Header/>
+      <main className="flex-1">{children}</main>
+         <Footer/>
+       </div>
   );
 }
