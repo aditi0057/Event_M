d1b6fa739c3591d6b8db7e5ae68caa0a770fd568
aditi0057 <aaditi_be22@thapar.edu>
2026-05-17 17:41:12 +0530

    Phase3 corrections

--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useState, useEffect } from 'react';
 import { headerLinks } from '@/constants';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
